67ac30b3cf1f7b1bf3fa8d8989b9ce63685500ab
Peter Hardy <peter@dropbear.id.au>
2020-01-27 20:59:27 +1100

    Concatenate gstreamer scripts, use single docker container for both.

---
+++ bin/gstreamer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+gst-launch-1.0 -v rpicamsrc name=src preview=0 exposure-mode=night \
+               fullscreen=0 bitrate=1000000 annotation-mode=time+date \
+               annotation-text-size=20 ! \
+               video/x-h264,width=960,height=540,framerate=12/1 ! \
+               queue max-size-bytes=0 max-size-buffers=0 ! h264parse ! \
+               rtph264pay config-interval=1 pt=96 ! queue ! \
+               udpsink host=127.0.0.1 port=5004 \
+               alsasrc device=hw:1 ! audioconvert ! audioresample ! \
+               opusenc ! rtpopuspay ! \
+               queue max-size-bytes=0 max-size-buffers=0 ! \
+               updsink host=127.0.0.1 port=5002
